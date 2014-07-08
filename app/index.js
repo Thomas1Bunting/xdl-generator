08b1950b85938e08fbe33a01774b9f9edd283f6d
Thomas Bunting <thomas.bunting@industrieit.com>
2014-07-08 11:31:55 +1000

    Changed it to be more like yo webapp

--- app/index.js
+++ app/index.js
@@ -1,58 +1,183 @@
 'use strict';
 var util = require('util');
 var path = require('path');
+var spawn = require('child_process').spawn;
 var yeoman = require('yeoman-generator');
-var yosay = require('yosay');
 var chalk = require('chalk');
 
 
-var XdlGenerator = yeoman.generators.Base.extend({
-  init: function () {
-    this.pkg = require('../package.json');
-      this.argument('appname', { type: String, required: false });
-      this.appname = this.appname || path.basename(process.cwd());
-      this.appname = this._.camelize(this._.slugify(this._.humanize(this.appname)));
+var AppGenerator = module.exports = function Appgenerator(args, options, config) {
+    yeoman.generators.Base.apply(this, arguments);
 
+    // setup the test-framework property, Gruntfile template will need this
+    this.testFramework = options['test-framework'] || 'mocha';
 
-      this.on('end', function () {
-      if (!this.options['skip-install']) {
-        this.installDependencies();
-      }
+    // for hooks to resolve on mocha by default
+    options['test-framework'] = this.testFramework;
+
+    // resolved to mocha by default (could be switched to jasmine for instance)
+    this.hookFor('test-framework', {
+        as: 'app',
+        options: {
+            options: {
+                'skip-message': options['skip-install-message'],
+                'skip-install': options['skip-install']
+            }
+        }
     });
-  },
 
-  askFor: function () {
-    var done = this.async();
+    this.options = options;
 
-    // Have Yeoman greet the user.
-    this.log(yosay('Welcome to the marvelous Xdl generator!'));
+    this.pkg = JSON.parse(this.readFileAsString(path.join(__dirname, '../package.json')));
+};
+
+util.inherits(AppGenerator, yeoman.generators.Base);
+
+AppGenerator.prototype.askFor = function askFor() {
+    var cb = this.async();
+
+    // welcome message
+    if (!this.options['skip-welcome-message']) {
+        console.log(this.yeoman);
+        console.log(chalk.magenta('Out of the box I include HTML5 Boilerplate, jQuery, and a Gruntfile.js to build your app.'));
+    }
 
     var prompts = [{
-      type: 'confirm',
-      name: 'someOption',
-      message: 'Would you like to enable this option?',
-      default: true
+        type: 'checkbox',
+        name: 'features',
+        message: 'What more would you like?',
+        choices: [{
+            name: 'Bootstrap',
+            value: 'includeBootstrap',
+            checked: true
+        },{
+            name: 'Sass',
+            value: 'includeSass',
+            checked: false
+        },{
+            name: 'Modernizr',
+            value: 'includeModernizr',
+            checked: false
+        }]
+    }, {
+        when: function (answers) {
+            return answers.features.indexOf('includeSass') !== -1;
+        },
+        type: 'confirm',
+        name: 'libsass',
+        value: 'includeLibSass',
+        message: 'Would you like to use libsass? Read up more at \n' + chalk.green('https://github.com/andrew/node-sass#reporting-sass-compilation-and-syntax-issues'),
+        default: false
     }];
 
-    this.prompt(prompts, function (props) {
-      this.someOption = props.someOption;
+    this.prompt(prompts, function (answers) {
+        var features = answers.features;
 
-      done();
+        function hasFeature(feat) { return features.indexOf(feat) !== -1; }
+        // manually deal with the response, get back and store the results.
+        // we change a bit this way of doing to automatically do this in the self.prompt() method.
+        this.includeSass = hasFeature('includeSass');
+        this.includeBootstrap = hasFeature('includeBootstrap');
+        this.includeModernizr = hasFeature('includeModernizr');
+
+        this.includeLibSass = answers.libsass;
+        this.includeRubySass = !(answers.libsass);
+
+        cb();
     }.bind(this));
-  },
+};
 
-  app: function () {
-    this.mkdir('app');
-    this.mkdir('app/templates');
+AppGenerator.prototype.gruntfile = function gruntfile() {
+    this.template('Gruntfile.js');
+};
 
-    this.copy('_package.json', 'package.json');
+AppGenerator.prototype.packageJSON = function packageJSON() {
+    this.template('_package.json', 'package.json');
+};
+
+AppGenerator.prototype.git = function git() {
+    this.copy('gitignore', '.gitignore');
+    this.copy('gitattributes', '.gitattributes');
+};
+
+AppGenerator.prototype.bower = function bower() {
     this.copy('_bower.json', 'bower.json');
-  },
+};
 
-  projectfiles: function () {
-    this.copy('editorconfig', '.editorconfig');
+AppGenerator.prototype.jshint = function jshint() {
     this.copy('jshintrc', '.jshintrc');
-  }
-});
+};
 
-module.exports = XdlGenerator;
+AppGenerator.prototype.editorConfig = function editorConfig() {
+    this.copy('editorconfig', '.editorconfig');
+};
+
+AppGenerator.prototype.h5bp = function h5bp() {
+    this.copy('favicon.ico', 'app/favicon.ico');
+    this.copy('404.html', 'app/404.html');
+    this.copy('robots.txt', 'app/robots.txt');
+    this.copy('htaccess', 'app/.htaccess');
+};
+
+AppGenerator.prototype.mainStylesheet = function mainStylesheet() {
+    var css = 'main.' + (this.includeSass ? 's' : '') + 'css';
+    this.copy(css, 'app/styles/' + css);
+};
+
+AppGenerator.prototype.writeIndex = function writeIndex() {
+
+    this.indexFile = this.readFileAsString(path.join(this.sourceRoot(), 'index.html'));
+    this.indexFile = this.engine(this.indexFile, this);
+
+    // wire Bootstrap plugins
+    if (this.includeBootstrap) {
+        var bs = '../bower_components/bootstrap' + (this.includeSass ? '-sass-official/vendor/assets/javascripts/bootstrap/' : '/js/');
+        this.indexFile = this.appendScripts(this.indexFile, 'scripts/plugins.js', [
+                bs + 'affix.js',
+                bs + 'alert.js',
+                bs + 'dropdown.js',
+                bs + 'tooltip.js',
+                bs + 'modal.js',
+                bs + 'transition.js',
+                bs + 'button.js',
+                bs + 'popover.js',
+                bs + 'carousel.js',
+                bs + 'scrollspy.js',
+                bs + 'collapse.js',
+                bs + 'tab.js'
+        ]);
+    }
+
+    this.indexFile = this.appendFiles({
+        html: this.indexFile,
+        fileType: 'js',
+        optimizedPath: 'scripts/main.js',
+        sourceFileList: ['scripts/main.js'],
+        searchPath: '{app,.tmp}'
+    });
+};
+
+AppGenerator.prototype.app = function app() {
+    this.mkdir('app');
+    this.mkdir('app/scripts');
+    this.mkdir('app/styles');
+    this.mkdir('app/images');
+    this.write('app/index.html', this.indexFile);
+
+
+        this.write('app/scripts/main.js', 'console.log(\'\\\'Allo \\\'Allo!\');');
+
+};
+
+AppGenerator.prototype.install = function () {
+    if (this.options['skip-install']) {
+        return;
+    }
+
+    var done = this.async();
+    this.installDependencies({
+        skipMessage: this.options['skip-install-message'],
+        skipInstall: this.options['skip-install'],
+        callback: done
+    });
+};
